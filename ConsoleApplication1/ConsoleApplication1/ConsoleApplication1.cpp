0f2bd94f79bb5f9bac3da387894c1883660957ed
Wojciech Książek <wojtekksiazekk@gmail.com>
2025-04-24 15:50:25 +0200

    Zmiany na galezi test

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+    std::cout << "Test!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
